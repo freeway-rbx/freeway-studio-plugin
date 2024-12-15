a47dee6ac61dbc2f620eb464300f91370c1b2f2c
Max Ignatyev <mxxnsk@gmail.com>
2024-12-15 13:07:13 -0800

    move wirer model calculation to a helper script

--- src/Plugin/PieceDetailsComponent.lua
+++ src/Plugin/PieceDetailsComponent.lua
@@ -8,12 +8,11 @@ local Selection = game:GetService("Selection")
 local CollectionService = game:GetService("CollectionService")
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
 local PieceDetailsComponent = React.Component:extend("PieceDetailsComponent")
-local WireableProperties = require(script.Parent.WireableProperties)
 
 local InstanceWirerComponent = require(script.Parent.InstanceWirerComponent)
 local PluginEnum = require(script.Parent.Enum)
 local t_u = require(script.Parent.tags_util)
-
+local ui_commons = require(script.Parent.ui_commons)
 
 function PieceDetailsComponent:didMount()
 	-- print('PieceDetailsComponent:didMount', self.state.selectedWirersModel)
@@ -51,72 +50,8 @@ function PieceDetailsComponent:init()
 
 end
 
-function PieceDetailsComponent:buildWirersModel(instances, pieceType) 
-	local result = {}
-	for _, instance in instances do
-		local wirerModelByType = result[instance.ClassName]
-		if wirerModelByType == nil then 
-			local properties = nil
-			if (pieceType == 'image') then
-				properties = WireableProperties['texture'][instance.ClassName]
-			elseif (pieceType == 'mesh') then 
-				properties = WireableProperties['mesh'][instance.ClassName]
-			end
-
-			--if properties == nil then properties = {} end
-			if properties == nil then continue end -- this instance can't be wired
-			wirerModelByType = {
-				instances = {},
-				properties = properties
-			} 
-			result[instance.ClassName] = wirerModelByType
-		end
-		table.insert(wirerModelByType.instances, instance)
-	end
-
-
-	for className, wirerModel in result do
-		local count = #wirerModel.instances
-		if count > 1 
-			then wirerModel.header = count .. ' ' .. className  .. 's' 
-			else wirerModel.header = wirerModel.instances[1].Name
-		end
-
-
-		-- per-property wiring state -- wired to current, not wired, etc
-		local properties_wire_state = {}
-		for j, instance in wirerModel.instances do
-			local instanceWires = t_u:get_instance_wires(instance)
-			for piece_id, property in instanceWires do
-				local prop_wire_st = properties_wire_state[property]
-				if prop_wire_st == nil then prop_wire_st = {} end
-				prop_wire_st[piece_id] = true
-				properties_wire_state[property] = prop_wire_st
-			end
-		end
-
-
-		for _, property in wirerModel.properties do -- add empty for properties that are not wired
-			if properties_wire_state[property] == nil then properties_wire_state[property]  = {} end
-		end
-
-		wirerModel.combinedPropertyState = {}
-		for property, wire_state in properties_wire_state do
-			local count = t_u:table_size(wire_state)
-			-- print('property->wireState', property, wire_state, count)
-			
-			if count == 0 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_NOT continue  end
-			if count > 1 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_MIXED continue end
-			
-			if wire_state[self.props.piece.id] then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_CURRENT
-			else 
-				wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_OTHER 
-				wirerModel.combinedPropertyState['piece_id_' .. property] = Cryo.Dictionary.keys(wire_state)[1]
-			end
-		end	
-		-- print('!!wireState', wirerModel.combinedPropertyState)
-	end
-	return result
+function PieceDetailsComponent:buildWirersModel(instances) 
+	return ui_commons:buildWirersModel(instances, self.props.piece.type, self.props.piece.id)
 end
 
 function PieceDetailsComponent:updateDMWirerState()
@@ -125,14 +60,14 @@ function PieceDetailsComponent:updateDMWirerState()
 	for instance, wires in instancesToWires do
 		if wires[self.props.piece.id] ~= nil then table.insert(instancesWiredToCurrentPiece, instance) end
 	end
-	local result = self:buildWirersModel(instancesWiredToCurrentPiece, self.props.piece.type)
+	local result = self:buildWirersModel(instancesWiredToCurrentPiece)
 	self:setState({dmWirersModel = result})
 end
 
 
 function PieceDetailsComponent:updateSelectedWirersState()
 	local selection = Selection:Get()
-	local result = self:buildWirersModel(selection, self.props.piece.type)
+	local result = self:buildWirersModel(selection)
 	self:setState({selectedWirersModel = result})
 end
 
---
+++ src/Plugin/ui_commons.lua
@@ -0,0 +1,82 @@
+local Freeway = script:FindFirstAncestor("Freeway")
+local Packages = Freeway.Packages
+local Cryo = require(Packages.Cryo)
+
+
+local WireableProperties = require(script.Parent.WireableProperties)
+local InstanceWirerComponent = require(script.Parent.InstanceWirerComponent)
+local PluginEnum = require(script.Parent.Enum)
+local t_u = require(script.Parent.tags_util)
+
+
+local ui_commons = {}
+
+function ui_commons:buildWirersModel(instances, pieceType, pieceId) 
+    local result = {}
+	for _, instance in instances do
+		local wirerModelByType = result[instance.ClassName]
+		if wirerModelByType == nil then 
+			local properties = nil
+			if (pieceType == 'image') then
+				properties = WireableProperties['texture'][instance.ClassName]
+			elseif (pieceType == 'mesh') then 
+				properties = WireableProperties['mesh'][instance.ClassName]
+			end
+
+			--if properties == nil then properties = {} end
+			if properties == nil then continue end -- this instance can't be wired
+			wirerModelByType = {
+				instances = {},
+				properties = properties
+			} 
+			result[instance.ClassName] = wirerModelByType
+		end
+		table.insert(wirerModelByType.instances, instance)
+	end
+
+
+	for className, wirerModel in result do
+		local count = #wirerModel.instances
+		if count > 1 
+			then wirerModel.header = count .. ' ' .. className  .. 's' 
+			else wirerModel.header = wirerModel.instances[1].Name
+		end
+
+
+		-- per-property wiring state -- wired to current, not wired, etc
+		local properties_wire_state = {}
+		for j, instance in wirerModel.instances do
+			local instanceWires = t_u:get_instance_wires(instance)
+			for piece_id, property in instanceWires do
+				local prop_wire_st = properties_wire_state[property]
+				if prop_wire_st == nil then prop_wire_st = {} end
+				prop_wire_st[piece_id] = true
+				properties_wire_state[property] = prop_wire_st
+			end
+		end
+
+
+		for _, property in wirerModel.properties do -- add empty for properties that are not wired
+			if properties_wire_state[property] == nil then properties_wire_state[property]  = {} end
+		end
+
+		wirerModel.combinedPropertyState = {}
+		for property, wire_state in properties_wire_state do
+			local count = t_u:table_size(wire_state)
+			-- print('property->wireState', property, wire_state, count)
+			
+			if count == 0 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_NOT continue  end
+			if count > 1 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_MIXED continue end
+			
+			if wire_state[pieceId] then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_CURRENT
+			else 
+				wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_OTHER 
+				wirerModel.combinedPropertyState['piece_id_' .. property] = Cryo.Dictionary.keys(wire_state)[1]
+			end
+		end	
+		-- print('!!wireState', wirerModel.combinedPropertyState)
+	end
+	return result
+end
+
+return ui_commons
