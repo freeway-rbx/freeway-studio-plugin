94f02c2f7f2926a557086e3d71fb3db518563c1b
Max Ignatyev <mxxnsk@gmail.com>
2024-12-17 16:40:47 -0800

    Fix wirer model logic

--- src/Plugin/ui_commons.lua
+++ src/Plugin/ui_commons.lua
@@ -11,6 +11,22 @@ local t_u = require(script.Parent.tags_util)
 
 local ui_commons = {}
 
+
+function ui_commons:buildWireableModelsForListMode(instances)
+	local wireables = {}
+	for _, instance in instances do
+		if WireableProperties['texture'][instance.ClassName] ~= nil or 
+		WireableProperties['mesh'][instance.ClassName] ~= nil then
+			table.insert(wireables, instance)
+		end
+	end	
+
+	local wirersModelMeshes = self:buildWirersModel(wireables, 'mesh')
+	local wirersModelImages = self:buildWirersModel(wireables, 'image')
+	return {wirersModelMeshes, wirersModelImages}
+end
+
+
 function ui_commons:buildWirersModel(instances, pieceType, pieceId) 
     local result = {}
 	for _, instance in instances do
@@ -57,6 +73,7 @@ function ui_commons:buildWirersModel(instances, pieceType, pieceId)
 
 
 		for _, property in wirerModel.properties do -- add empty for properties that are not wired
+			
 			if properties_wire_state[property] == nil then properties_wire_state[property]  = {} end
 		end
 
@@ -68,10 +85,19 @@ function ui_commons:buildWirersModel(instances, pieceType, pieceId)
 			if count == 0 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_NOT continue  end
 			if count > 1 then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_MIXED continue end
 			
-			if wire_state[pieceId] then wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_CURRENT
-			else 
-				wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_OTHER 
-				wirerModel.combinedPropertyState['piece_id_' .. property] = Cryo.Dictionary.keys(wire_state)[1]
+			-- if only wired to one piece id: 
+			if pieceId ~= nil then
+				if wire_state[pieceId] and #wirerModel.instances == count then 
+				   print("propwirestate", property, 'ALL_CURRENT')
+					wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_CURRENT continue
+				elseif wire_state[pieceId] then 
+					print("propwirestate", property, 'WIRED_TO_CURRENT_AND_UNWIRED')
+					wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_TO_CURRENT_AND_UNWIRED continue	
+				else 
+					print("propwirestate", property, 'WIRED_ALL_OTHER')
+					wirerModel.combinedPropertyState[property] = PluginEnum.WIRED_ALL_OTHER 
+					wirerModel.combinedPropertyState['piece_id_' .. property] = Cryo.Dictionary.keys(wire_state)[1]
+				end 
 			end
 		end	
 		-- print('!!wireState', wirerModel.combinedPropertyState)
